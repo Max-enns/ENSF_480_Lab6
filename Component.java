8bf55c5a20b4c7a0dcd68946e3d45198ba2f24b3
Max-enns <144195868+Max-enns@users.noreply.github.com>
2024-11-04 15:39:09 -0700

    added component

---
+++ Component.java
@@ -0,0 +1,13 @@
+/*
+* File Name: Decorator.java
+* Assignment: Lab 6 Exercise A and B
+* Lab Section: B01
+* Completed by: Max Enns
+* Submission Date: November 4th, 2024
+*/
+
+import java.awt.*;
+
+public interface Component {
+    public abstract void draw(Graphics g);
+}
